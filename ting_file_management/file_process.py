7ccd106dbf69bd362a299485a03abd0a22f99f9a
PhilipLages <psilvaelages@gmail.com>
2023-05-15 18:22:49 -0300

    feat: Implements process
    Req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,9 +1,20 @@
-from file_management import txt_importer
-from queue import Queue
+import sys
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
 
 
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+def process(path_file, instance: Queue):
+    if path_file not in instance.queue:
+        instance.enqueue(path_file)
+
+        file = txt_importer(path_file)
+        file_info = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file),
+            "linhas_do_arquivo": file
+        }
+
+        sys.stdout.write(str(file_info))
 
 
 def remove(instance):

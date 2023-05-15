517b2d48681995eb6c6ed293cd6d68376ce47c58
PhilipLages <psilvaelages@gmail.com>
2023-05-15 19:13:21 -0300

    feat: Implements remove
    Req 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -17,8 +17,13 @@ def process(path_file, instance: Queue):
         sys.stdout.write(str(file_info))
 
 
-def remove(instance):
-    """Aqui irá sua implementação"""
+def remove(instance: Queue):
+    if instance.__len__() == 0:
+        sys.stdout.write("Não há elementos\n")
+
+    removed = instance.dequeue()
+    if removed is not None:
+        sys.stdout.write(f'Arquivo {removed} removido com sucesso\n')
 
 
 def file_metadata(instance, position):

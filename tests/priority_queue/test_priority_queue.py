3c6807171dd61b74f68b3e1243783e6cab52076e
PhilipLages <psilvaelages@gmail.com>
2023-05-15 21:46:13 -0300

    chore: Changing indentation, again...

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -18,23 +18,21 @@ def queue():
 
 
 def test_basic_priority_queueing(files, queue):
-    queue.enqueue(files[0])
-    queue.enqueue(files[1])
-    assert queue.regular_priority.queue == [files[0]]
-    assert queue.high_priority.queue == [files[1]]
-    assert queue.search(1) == files[0]
+	queue.enqueue(files[0])
+	queue.enqueue(files[1])
+	assert queue.regular_priority.queue == [files[0]]
+	assert queue.high_priority.queue == [files[1]]
+	assert queue.search(1) == files[0]
 
-    queue.enqueue(files[2])
-    queue.enqueue(files[3])
+	queue.enqueue(files[2])
+	queue.enqueue(files[3])
 
-    assert queue.search(0) == files[1]
+	assert queue.search(0) == files[1]
 
-    with raises(
-      IndexError, match="Índice Inválido ou Inexistente"
-    ):
-      queue.search(6)
+	with raises(IndexError, match="Índice Inválido ou Inexistente"):
+		queue.search(6)
 
-    assert queue.dequeue() == files[1]
-    assert queue.dequeue() == files[3]
-    assert queue.dequeue() == files[0]
-    assert queue.dequeue() == files[2]
+	assert queue.dequeue() == files[1]
+	assert queue.dequeue() == files[3]
+	assert queue.dequeue() == files[0]
+	assert queue.dequeue() == files[2]

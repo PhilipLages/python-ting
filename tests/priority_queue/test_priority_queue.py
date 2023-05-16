c58f0b008e9c5e9d50b9a267da458f4b74e282d7
PhilipLages <psilvaelages@gmail.com>
2023-05-15 21:42:33 -0300

    chore: Changes indentation

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -4,37 +4,37 @@ from pytest import fixture, raises
 
 @fixture
 def files():
-  return [      
-    {"qtd_linhas": 8},
-    {"qtd_linhas": 3},
-    {"qtd_linhas": 6},
-    {"qtd_linhas": 2}
-  ]
+    return [      
+      {"qtd_linhas": 8},
+      {"qtd_linhas": 3},
+      {"qtd_linhas": 6},
+      {"qtd_linhas": 2}
+    ]
 
 
 @fixture
 def queue():
-  return PriorityQueue()
+    return PriorityQueue()
 
 
 def test_basic_priority_queueing(files, queue):
-  queue.enqueue(files[0])
-  queue.enqueue(files[1])
-  assert queue.regular_priority.queue == [files[0]]
-  assert queue.high_priority.queue == [files[1]]
-  assert queue.search(1) == files[0]
+    queue.enqueue(files[0])
+    queue.enqueue(files[1])
+    assert queue.regular_priority.queue == [files[0]]
+    assert queue.high_priority.queue == [files[1]]
+    assert queue.search(1) == files[0]
 
-  queue.enqueue(files[2])
-  queue.enqueue(files[3])
+    queue.enqueue(files[2])
+    queue.enqueue(files[3])
 
-  assert queue.search(0) == files[1]
+    assert queue.search(0) == files[1]
 
-  with raises(
-    IndexError, match="Índice Inválido ou Inexistente"
-  ):
-    queue.search(6)
+    with raises(
+      IndexError, match="Índice Inválido ou Inexistente"
+    ):
+      queue.search(6)
 
-  assert queue.dequeue() == files[1]
-  assert queue.dequeue() == files[3]
-  assert queue.dequeue() == files[0]
-  assert queue.dequeue() == files[2]
+    assert queue.dequeue() == files[1]
+    assert queue.dequeue() == files[3]
+    assert queue.dequeue() == files[0]
+    assert queue.dequeue() == files[2]
